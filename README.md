4102a48fd2a9180d136a770b4b47f92232379e19
arshak91 <arshakgasparyan091@gmail.com>
2025-05-14 18:30:25 +0400

    changes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # newProject
+
+## run project
+
+git clone ....
+cd ....
+npm i
+npm run dev // for developers
+npm run start // for production
---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+require("dotenv/config")
+const arr = [1, 2, 3, 4 , 5];
+(function name(args) {
+  console.log("run name function",process, process.env.PORT, process.env.API_KEY);
+})()
+
+
+foo()
+
+console.log(arr);
+
